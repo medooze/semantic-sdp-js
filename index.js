6461af43372708057328a4f6f90677322d91ffc7
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2017-02-20 16:38:33 +0100

    Initil commit

---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports =
+{
+	SDPInfo			: require("./lib/CandidateInfo"),
+	CandidateInfo		: require("./lib/CandidateInfo"),
+	CodecInfo		: require("./lib/CodecInfo"),
+	DTLSInfo		: require("./lib/DTLSInfo"),
+	ICEInfo			: require("./lib/ICEInfo"),
+	MediaInfo		: require("./lib/MediaInfo"),
+	Setup			: require("./lib/Setup"),
+	SourceGroupInfo		: require("./lib/SourceGroupInfo"),
+	SourceInfo		: require("./lib/SourceInfo"),
+	StreamInfo		: require("./lib/StreamInfo"),
+	TrackInfo		: require("./lib/TrackInfo")
+};
