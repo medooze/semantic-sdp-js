01193f0671cd2e0fb50e0156330bee5c963a924b
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2021-09-13 11:24:30 +0200

    add support for ice at session level

--- test/fragment.js
+++ test/fragment.js
@@ -31,3 +31,25 @@ const sdpInfo = SDPInfo.create({
 
 
 console.log(sdpInfo.toIceFragmentString());
+
+
+const sdp = "a=ice-ufrag:cB7q\r\n" +
+	"a=ice-pwd:DtBRorOM5cdJTSAN8lDqIpqI\r\n" +
+	"m=audio RTP/AVP 0\r\n" +
+	"a=mid:0\r\n" +
+	"a=candidate:1387637174 1 udp 2122260223 192.168.0.39 52564 typ host generation 0 ufrag MS47 network-id 1\r\n" +
+	"a=candidate:3471623853 1 udp 2122194687 192.168.64.1 52565 typ host generation 0 ufrag MS47 network-id 2\r\n" +
+	"a=end-of-candiadates\r\n" +
+	"m=video RTP/AVP 0\r\n" +
+	"a=mid:1\r\n" +
+	"a=candidate:1387637174 1 udp 2122260223 192.168.0.39 52566 typ host generation 0 ufrag MS47 network-id 1\r\n" +
+	"a=candidate:3471623853 1 udp 2122194687 192.168.64.1 52567 typ host generation 0 ufrag MS47 network-id 2\r\n" +
+	"a=end-of-candiadates\r\n";
+
+
+//Process the sdp
+var offer = SDPInfo.process(sdp);
+
+console.log("Offer:"+offer.toIceFragmentString());
+console.dir(offer, {depth: null, colors: true});
+console.dir(offer.plain(), {depth: null, colors: true});
