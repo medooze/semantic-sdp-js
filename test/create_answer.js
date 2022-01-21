07374c5d82f415a806ebf78b146063e377168dd8
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2022-01-21 16:31:56 +0100

    Support mixed and not mixed extmaps

--- test/create_answer.js
+++ test/create_answer.js
@@ -170,3 +170,45 @@ const answer2 = sdpInfo2.answer({
 
 console.dir(answer2.plain(),{depth: null, colors: true});
 console.log(answer2.toString());
+
+//Set extmap
+sdpInfo2.setExtmapAllowMixed(false);
+
+const answer2noextmap = sdpInfo2.answer({
+	dtls		: DTLSInfo.expand({
+		"hash"        : "sha-256",
+		"fingerprint" : "F2:AA:0E:C3:22:59:5E:14:95:69:92:3D:13:B4:84:24:2C:C2:A2:C0:3E:FD:34:8E:5E:EA:6F:AF:52:CE:E6:0F"
+	}),
+	ice		: ICEInfo.expand({
+		"ufrag" : "af46F",
+		"pwd"   : "a34FasdS++jdfofdslkjsd\/SDV"
+	}),
+	candidates	: [CandidateInfo.expand({
+		"foundation"	: 1,
+		"componentId"	: 1,
+		"transport"	: "udp",
+		"priority"	: 2122260223,
+		"address"	: "192.168.0.196",
+		"port"		: 56143,
+		"type"		: "host"
+	})],
+	capabilities	: {
+		audio : {
+			codecs		: CodecInfo.MapFromNames(["opus"]),
+			extensions	: ["urn:ietf:params:rtp-hdrext:ssrc-audio-level","http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01"],
+		},
+		video : {
+			codecs		: CodecInfo.MapFromNames(["vp9"],true,[
+				{ "id": "transport-cc"},
+				{ "id": "ccm", "params": ["fir"]},
+				{ "id": "nack"},
+				{ "id": "nack", "params": ["pli"]}
+			]),
+			extensions	: [ "urn:3gpp:video-orientation", "http://www.ietf.org/id/draft-holmer-rmcat-transport-wide-cc-extensions-01"]
+		}
+	}
+});
+
+
+console.dir(answer2noextmap.plain(),{depth: null, colors: true});
+console.log(answer2noextmap.toString());
